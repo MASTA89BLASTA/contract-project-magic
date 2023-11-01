9d4046ac5bc968faf06b405da4f75a23b867771d
Daniil-ops <daniil252000@mail.ru>
2023-11-01 23:52:21 +0300

    test 3.1

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,17 +5,17 @@ module.exports = function Main() {
   return (
     <Layout>
       <form className="form-main">
-        <input checked="<%= true %>" id="signin" className="action" type="radio" value="signin" />
+        <input checked="<%= true %>" id="signin" name="action" type="radio" value="signin" />
         <label htmlFor="signin">Sign in</label>
-        <input id="signup" className="action" type="radio" value="signup" />
+        <input id="signup" name="action" type="radio" value="signup" />
         <label htmlFor="signup">Sign up</label>
-        <input id="reset" className="action" type="radio" value="reset" />
+        <input id="reset" name="action" type="radio" value="reset" />
         <label htmlFor="reset">Reset</label>
         <div id="wrapper">
           <div id="arrow" />
-          <input id="email" placeholder="Email" type="text" />
-          <input id="pass" placeholder="Password" type="password" />
-          <input id="repass" placeholder="Repeat password" type="password" />
+          <input id="email" name="email" placeholder="Email" type="text" />
+          <input id="pass" name="pass" placeholder="Password" type="password" />
+          <input id="repass" name="repass" placeholder="Repeat password" type="password" />
         </div>
         <button type="submit">
           <span>
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,35 +2,39 @@ const formRegister = document.querySelector(".form-main");
 const radioButtons = document.getElementsByName(".action");
 const selectedOption = "";
 radioButtons.forEach((radioButton) => {
-  console.log(radioButton);
+  if (radioButton.checked) {
+    selectedOption = radioButton.value;
+    console.log(selectedOption);
+  }
 });
 if (formRegister) {
   formRegister.addEventListener("submit", async (event) => {
     event.preventDefault();
+    const { email, pass, repass } = event.target;
+    console.log(email);
 
-    // const { login, email, password } = event.target;
     // event.target = то, что вызвало событие - то есть наша форма
 
     // 2 шаг отправить запрос на сервер
-    // const response = await fetch("/register", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     login: login.value,
-    //     email: email.value,
-    //     password: password.value,
-    //   }),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
+    const response = await fetch("/", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email.value,
+        pass: pass.value,
+        repass: repass.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    // window.location.assign("/login");
+    window.location.assign("/login");
 
-    // const data = await response.json();
+    const data = await response.json();
 
-    // // понять, куда вставлять
-    // const container = document.querySelector(".js-facts-container");
-    // // что вставлять
-    // container.insertAdjacentHTML("beforebegin", data.cardHtml);
+    // понять, куда вставлять
+    const container = document.querySelector(".js-facts-container");
+    // что вставлять
+    container.insertAdjacentHTML("beforebegin", data.cardHtml);
   });
 }
