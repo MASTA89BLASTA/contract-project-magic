d304f7a94629431739e3e20977c70125a4c2fac1
Daniil-ops <daniil252000@mail.ru>
2023-11-02 00:16:48 +0300

    test 4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,28 @@
 const formRegister = document.querySelector(".form-main");
-const radioButtons = document.getElementsByName(".action");
-const selectedOption = "";
-radioButtons.forEach((radioButton) => {
-  if (radioButton.checked) {
-    selectedOption = radioButton.value;
-    console.log(selectedOption);
-  }
-});
+// const radioButtons = document.getElementsByName(".action");
+// const selectedOption = "";
+// radioButtons.forEach((radioButton) => {
+//   if (radioButton.checked) {
+//     selectedOption = radioButton.value;
+//     // console.log(selectedOption);
+//   }
+// });
 if (formRegister) {
   formRegister.addEventListener("submit", async (event) => {
     event.preventDefault();
+
+    const radioButtons = document.getElementsByName("action");
+    let selectedOption = "";
+
+    Array.from(radioButtons).forEach((radioButton) => {
+      if (radioButton.checked) {
+        selectedOption = radioButton.value;
+      }
+    });
+
+    console.log(selectedOption);
+
     const { email, pass, repass } = event.target;
-    console.log(email);
 
     // event.target = то, что вызвало событие - то есть наша форма
 
@@ -27,8 +38,9 @@ if (formRegister) {
         "Content-Type": "application/json",
       },
     });
+    console.log(radioButtons);
 
-    window.location.assign("/login");
+    // window.location.assign("/login");
 
     const data = await response.json();
 
