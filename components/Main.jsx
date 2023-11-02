53c5354136011786f8e1c7f5f9e37eb130f79609
Daniil-ops <daniil252000@mail.ru>
2023-11-02 16:33:25 +0300

    test 6

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,18 +4,33 @@ const Layout = require("./Layout");
 module.exports = function Main() {
   return (
     <Layout>
-      <form className="form-main">
+      <body className="body-main">
+        <form className="form-main">
+          <div className="top-dev-main">
+            <div>logo</div>
+            <div>favorites</div>
+            <div>avatarka</div>
+            <div>clear</div>
+          </div>
+          <div className="top-dev-center">
+            <div className="limitirovan-full">
+              <div className="block">Лимитораванная карта</div>
+              <div className="block">Доминик Торетто</div>
+              <button className="button-limitirovannay" type="submit">Купить сейчас</button>
+            </div>
+            <div>favorites</div>
+          </div>
 
-        <div id="wrapper">
-          <div id="arrow" />
-          <input className="login" name="login" placeholder="Login" type="text" />
-          <input className="email" name="email" placeholder="Email" type="text" />
-          <input className="password" name="password" placeholder="Password" type="password" />
-          <input className="repass" name="repass" placeholder="Repeat password" type="password" />
-        </div>
-
-        {/* <script defer src="./js/scripts.js" /> */}
-      </form>
+          <div id="wrapper">
+            <div id="arrow" />
+            <input className="login" name="login" placeholder="Login" type="text" />
+            <input className="email" name="email" placeholder="Email" type="text" />
+            <input className="password" name="password" placeholder="Password" type="password" />
+            <input className="repass" name="repass" placeholder="Repeat password" type="password" />
+          </div>
+          {/* <script defer src="./js/scripts.js" /> */}
+        </form>
+      </body>
     </Layout>
   );
 };
--- components/register/FormRegistration.jsx
+++ components/register/FormRegistration.jsx
@@ -4,63 +4,65 @@ const Layout = require("../Layout");
 module.exports = function FormRegistration() {
   return (
     <Layout>
-      <form className="form-reg">
-        <input checked="<%= true %>" id="signin" name="action" type="radio" value="signin" />
-        <label htmlFor="signin">Sign in</label>
-        <input id="signup" name="action" type="radio" value="signup" />
-        <label htmlFor="signup">Sign up</label>
-        <input id="reset" name="action" type="radio" value="reset" />
-        <label htmlFor="reset">Reset</label>
-        <div id="wrapper">
-          <div id="arrow" />
-          <input className="login" name="login" placeholder="Login" type="text" />
-          <input className="email" name="email" placeholder="Email" type="text" />
-          <input className="password" name="password" placeholder="Password" type="password" />
-          <input className="repass" name="repass" placeholder="Repeat password" type="password" />
-        </div>
-        <button className="button-login" type="submit">
-          <span>
-            Reset password
-            <br />
-            Sign in
-            <br />
-            Sign up
-          </span>
-        </button>
-
-        <form>
-          <input type="reset" className="reset" value="Clear it!" />
-          <div className="__select">
-            <div className="__select__content">
-              <input id="singleSelect0" checked="<%= true %>" className="__select__input" type="radio" value="Москва" name="singleSelect" />
-              <label htmlFor="singleSelect0" className="__select__label">Москва</label>
-
-              <input id="singleSelect1" className="__select__input" type="radio" value="Санкт-Петербург" name="singleSelect" />
-              <label htmlFor="singleSelect1" className="__select__label">Санкт-Петербург</label>
-
-              <input id="singleSelect2" className="__select__input" type="radio" value="Новосибирск" name="singleSelect" />
-              <label htmlFor="singleSelect2" className="__select__label">Новосибирск</label>
-
-              <input id="singleSelect3" className="__select__input" type="radio" value="Казань" name="singleSelect" />
-              <label htmlFor="singleSelect3" className="__select__label">Казань</label>
-
-              <input id="singleSelect4" className="__select__input" type="radio" value="Воронеж" name="singleSelect" />
-              <label htmlFor="singleSelect4" className="__select__label">Воронеж</label>
-
-              <input id="singleSelect5" className="__select__input" type="radio" value="Екатеринбург" name="singleSelect" />
-              <label htmlFor="singleSelect5" className="__select__label">Екатеринбург</label>
-
-              <input id="singleSelect6" className="__select__input" type="radio" value="Саратов" name="singleSelect" />
-              <label htmlFor="singleSelect6" className="__select__label">Саратов</label>
-
-              <input id="singleSelect7" className="__select__input" type="radio" value="Энгельс" name="singleSelect" />
-              <label htmlFor="singleSelect7" className="__select__label">Энгельс</label>
-            </div>
+      <body className="body-reg">
+        <form className="form-reg">
+          <input checked="<%= true %>" id="signin" name="action" type="radio" value="signin" />
+          <label htmlFor="signin">Sign in</label>
+          <input id="signup" name="action" type="radio" value="signup" />
+          <label htmlFor="signup">Sign up</label>
+          <input id="reset" name="action" type="radio" value="reset" />
+          <label htmlFor="reset">Reset</label>
+          <div id="wrapper">
+            <div id="arrow" />
+            <input className="login" name="login" placeholder="Login" type="text" />
+            <input className="email" name="email" placeholder="Email" type="text" />
+            <input className="password" name="password" placeholder="Password" type="password" />
+            <input className="repass" name="repass" placeholder="Repeat password" type="password" />
           </div>
-        </form>
+          <button className="button-login" type="submit">
+            <span>
+              Reset password
+              <br />
+              Sign in
+              <br />
+              Sign up
+            </span>
+          </button>
 
-        <script defer src="./js/scripts.js" />
-      </form>
+          <form>
+            <input type="reset" className="reset" value="Clear it!" />
+            <div className="__select">
+              <div className="__select__content">
+                <input id="singleSelect0" checked="<%= true %>" className="__select__input" type="radio" value="Москва" name="singleSelect" />
+                <label htmlFor="singleSelect0" className="__select__label">Москва</label>
+
+                <input id="singleSelect1" className="__select__input" type="radio" value="Санкт-Петербург" name="singleSelect" />
+                <label htmlFor="singleSelect1" className="__select__label">Санкт-Петербург</label>
+
+                <input id="singleSelect2" className="__select__input" type="radio" value="Новосибирск" name="singleSelect" />
+                <label htmlFor="singleSelect2" className="__select__label">Новосибирск</label>
+
+                <input id="singleSelect3" className="__select__input" type="radio" value="Казань" name="singleSelect" />
+                <label htmlFor="singleSelect3" className="__select__label">Казань</label>
+
+                <input id="singleSelect4" className="__select__input" type="radio" value="Воронеж" name="singleSelect" />
+                <label htmlFor="singleSelect4" className="__select__label">Воронеж</label>
+
+                <input id="singleSelect5" className="__select__input" type="radio" value="Екатеринбург" name="singleSelect" />
+                <label htmlFor="singleSelect5" className="__select__label">Екатеринбург</label>
+
+                <input id="singleSelect6" className="__select__input" type="radio" value="Саратов" name="singleSelect" />
+                <label htmlFor="singleSelect6" className="__select__label">Саратов</label>
+
+                <input id="singleSelect7" className="__select__input" type="radio" value="Энгельс" name="singleSelect" />
+                <label htmlFor="singleSelect7" className="__select__label">Энгельс</label>
+              </div>
+            </div>
+          </form>
+
+          <script defer src="./js/scripts.js" />
+        </form>
+      </body>
     </Layout>
   );
 };
