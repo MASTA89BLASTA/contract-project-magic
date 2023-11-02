4d1e85d698ddd25e33fda2e6baa8fff4146906c2
Your Name <you@example.com>
2023-11-02 19:50:03 +0300

    get all Cards

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,10 +2,16 @@ const React = require("react");
 const Layout = require("./Layout");
 const Header = require("./Header");
 
-module.exports = function Main() {
+module.exports = function Main({ cards }) {
+  console.log(cards);
   return (
     <Layout>
       <Header />
+      <div className="js-card-container facts-container">
+        {cards.map(card => (
+          <img style={{ width: "200px" }} src={card.img} alt={card.name} />
+        ))}
+      </div>
     </Layout>
   );
 };
