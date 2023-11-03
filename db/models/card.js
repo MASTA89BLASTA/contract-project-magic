a1271a1bc380f81c6a69fe570290110b6b371308
Daniil-ops <daniil252000@mail.ru>
2023-11-03 11:14:36 +0300

    vetki

--- db/models/card.js
+++ db/models/card.js
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ User }) {
+      this.belongsTo(User, { foreignKey: "user_id" });
     }
   }
   Card.init({
@@ -29,6 +29,15 @@ module.exports = (sequelize, DataTypes) => {
     level: {
       allowNull: false,
       type: DataTypes.TEXT,
+    },
+    user_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onDelete: "cascade",
     },
   }, {
     sequelize,
--- db/seeders/20231102130157-cards.js
+++ db/seeders/20231102130157-cards.js
@@ -1,96 +1,102 @@
-"use strict";
-
-const { Card } = require("../models");
+const bcrypt = require("bcrypt");
+const { User, Card } = require("../models");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Card.bulkCreate([
-      {
-        name: "Adjany",
-        img: "/Cards/Аджани.jpg",
-        price: "200 tokens",
-        level: "New",
-      },
-      {
-        name: "Vivien",
-        img: "/Cards/Вивьен.jpg",
-        price: "100 tokens",
-        level: "Medium",
-      },
-      {
-        name: "Gideon",
-        img: "/Cards/Гидеон.jpg",
-        price: "180 tokens",
-        level: "New",
-      },
-      {
-        name: "Jase",
-        img: "/Cards/Джейс.jpg",
-        price: "200 tokens",
-        level: "New",
-      },
-      {
-        name: "Dominic",
-        img: "/Cards/Доминик.jpg",
-        price: "2000 tokens",
-        level: "Exclusive",
-      },
-      {
-        name: "Lilian",
-        img: "/Cards/Лилиана.jpg",
-        price: "300 tokens",
-        level: "New",
-      },
-      {
-        name: "Ob-Nyxlis",
-        img: "/Cards/Об-Никсилис.jpg",
-        price: "410 tokens",
-        level: "New",
-      },
-      {
-        name: "Renn",
-        img: "/Cards/Ренн.jpg",
-        price: "120 tokens",
-        level: "Old",
-      },
-      {
-        name: "Sorin",
-        img: "/Cards/Сорин.jpg",
-        price: "250 tokens",
-        level: "New",
-      },
-      {
-        name: "Stranger",
-        img: "/Cards/Странница.jpg",
-        price: "200 tokens",
-        level: "Medium",
-      },
-      {
-        name: "Tamyo",
-        img: "/Cards/Тамиё.jpg",
-        price: "300 tokens",
-        level: "New",
-      },
-      {
-        name: "Tezzered",
-        img: "/Cards/Теззерет.jpg",
-        price: "500 tokens",
-        level: "New",
-      },
-      {
-        name: "Tefery",
-        img: "/Cards/Тефери.jpg",
-        price: "150 tokens",
-        level: "Medium",
-      },
-      {
-        name: "Chandra",
-        img: "/Cards/Чандра.jpg",
-        price: "100 tokens",
-        level: "Old",
-      },
-    ]);
+    await User.create({
+      login: "lisa",
+      email: "lisa@mail.ru",
+      password: await bcrypt.hash("123", 10),
+      city: "Санкт Петербург",
+      cash: 1000,
+      Cards: [
+        {
+          name: "Adjany",
+          img: "/Cards/Аджани.jpg",
+          price: "200 tokens",
+          level: "New",
+        },
+        {
+          name: "Vivien",
+          img: "/Cards/Вивьен.jpg",
+          price: "100 tokens",
+          level: "Medium",
+        },
+        {
+          name: "Gideon",
+          img: "/Cards/Гидеон.jpg",
+          price: "180 tokens",
+          level: "New",
+        },
+        {
+          name: "Jase",
+          img: "/Cards/Джейс.jpg",
+          price: "200 tokens",
+          level: "New",
+        },
+        {
+          name: "Dominic",
+          img: "/Cards/Доминик.jpg",
+          price: "2000 tokens",
+          level: "Exclusive",
+        },
+        {
+          name: "Lilian",
+          img: "/Cards/Лилиана.jpg",
+          price: "300 tokens",
+          level: "New",
+        },
+        {
+          name: "Ob-Nyxlis",
+          img: "/Cards/Об-Никсилис.jpg",
+          price: "410 tokens",
+          level: "New",
+        },
+        {
+          name: "Renn",
+          img: "/Cards/Ренн.jpg",
+          price: "120 tokens",
+          level: "Old",
+        },
+        {
+          name: "Sorin",
+          img: "/Cards/Сорин.jpg",
+          price: "250 tokens",
+          level: "New",
+        },
+        {
+          name: "Stranger",
+          img: "/Cards/Странница.jpg",
+          price: "200 tokens",
+          level: "Medium",
+        },
+        {
+          name: "Tamyo",
+          img: "/Cards/Тамиё.jpg",
+          price: "300 tokens",
+          level: "New",
+        },
+        {
+          name: "Tezzered",
+          img: "/Cards/Теззерет.jpg",
+          price: "500 tokens",
+          level: "New",
+        },
+        {
+          name: "Tefery",
+          img: "/Cards/Тефери.jpg",
+          price: "150 tokens",
+          level: "Medium",
+        },
+        {
+          name: "Chandra",
+          img: "/Cards/Чандра.jpg",
+          price: "100 tokens",
+          level: "Old",
+        },
+      ],
+    });
   },
 
   async down(queryInterface, Sequelize) {
